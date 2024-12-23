2d7c618568ee511eb93cd3e4efeb29d565becacf
Kocherga <malinovnamasana@gmail.com>
2024-12-23 18:25:11 +0300

    first commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: postgresql
+
+	// TODO: init router: chi, chi render
+
+	// TODO: run server
+}
