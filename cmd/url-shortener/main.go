75ef2aec7b41be1e5d422a54d7c8627a62eb74c8
Kocherga <malinovnamasana@gmail.com>
2024-12-23 20:35:13 +0300

    slog logger setuped

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"url-shortener/cmd/internal/config"
 )
 
-func main() {
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
+func main() {
 	cfg := config.MustLoad()
 
-	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener")
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: postgresql
 
 	// TODO: init router: chi, chi render
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+
 }
